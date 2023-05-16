b056a01ec3c4cdede5cf0033aa35e529041dc1ce
Lucas Fernandes <lucas.fernandes.app@gmail.com>
2023-05-16 20:07:05 -0300

    chore: configs native wind

--- src/presentation/screens/Home/index.tsx
+++ src/presentation/screens/Home/index.tsx
@@ -3,7 +3,7 @@ import Header from '../../components/Header'
 
 const HomeScreen = () => {
   return (
-    <View>
+    <View className="flex justify-center items-center h-full">
       <Header />
       <Text>HomeScreen</Text>
       <Text>Lint and husky</Text>
