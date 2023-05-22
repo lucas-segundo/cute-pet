7297da79684f2bcd84471a8f924fbde6331c987f
Lucas Fernandes <lucas.fernandes.app@gmail.com>
2023-05-22 11:36:47 -0300

    test: should change category items style when one of the items is selected

---
+++ src/presentation/components/Category/test.tsx
@@ -0,0 +1,20 @@
+import '@testing-library/jest-native/extend-expect'
+import { screen, render, fireEvent } from '@testing-library/react-native'
+
+import Category from '.'
+
+import { expectNotToBeOutline, expectToBeOutline } from './Item/test'
+
+describe('Category', () => {
+  it('should change category items style when one of the items is selected', () => {
+    render(<Category />)
+
+    const catCategory = screen.getByTestId('catCategory')
+    const dogCategory = screen.getByTestId('dogCategory')
+
+    fireEvent.press(dogCategory)
+
+    expectToBeOutline(catCategory)
+    expectNotToBeOutline(dogCategory)
+  })
+})
