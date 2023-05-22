58f70d4d0801dc2c8eda34d258002a2ec15f5442
Lucas Fernandes <lucas.fernandes.app@gmail.com>
2023-05-22 08:19:18 -0300

    test: should render outline

--- src/presentation/components/Category/Item/test.tsx
+++ src/presentation/components/Category/Item/test.tsx
@@ -1,13 +1,36 @@
+import '@testing-library/jest-native/extend-expect'
 import { screen, render } from '@testing-library/react-native'
-import CategoryItem from '.'
+
 import { faker } from '@faker-js/faker'
 
+import CategoryItem from '.'
+
+const getMockValues = () => {
+  const text = faker.lorem.text()
+
+  return {
+    text,
+  }
+}
+
 describe('CategoryItem', () => {
-  it('should render with props correctly', () => {
-    const text = faker.lorem.text()
+  it('should render with text', () => {
+    const { text } = getMockValues()
 
     render(<CategoryItem text={text} />)
 
-    expect(screen.getByText(text)).toBeTruthy()
+    const component = screen.getByText(text)
+    expect(component).toBeTruthy()
+  })
+
+  it('should render outline', () => {
+    const { text } = getMockValues()
+
+    render(<CategoryItem text={text} isOutline />)
+
+    const component = screen.getByTestId('categoryItemContainer')
+    expect(component).not.toHaveStyle({
+      backgroundColor: '#6366f1',
+    })
   })
 })
