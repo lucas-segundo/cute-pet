11a69e67915bf8186413b749c8e70eea14536904
Lucas Fernandes <lucas.fernandes.app@gmail.com>
2023-05-23 18:53:33 -0300

    test: should render with animal props correctly

---
+++ src/presentation/components/AnimalCard/test.tsx
@@ -0,0 +1,41 @@
+import '@testing-library/jest-native/extend-expect'
+import { screen, render } from '@testing-library/react-native'
+import AnimalCard from '.'
+import { mockAnimal } from '../../../domain/models/Animal/mock'
+
+const mockParams = () => {
+  const animal = mockAnimal()
+  const containerClassName = 'mt-2'
+
+  return {
+    animal,
+    containerClassName,
+  }
+}
+
+describe('AnimalCard', () => {
+  it('should render with animal props correctly', () => {
+    const { animal } = mockParams()
+
+    render(<AnimalCard animal={animal} />)
+
+    expect(screen.getByText(animal.name)).toBeTruthy()
+    expect(screen.getByText(animal.race)).toBeTruthy()
+    expect(screen.getByTestId('animalPhoto')).toHaveProperty(
+      'props.source.uri',
+      animal.photos[0].url
+    )
+  })
+
+  it('should render with correct containerClassName', () => {
+    const { animal, containerClassName } = mockParams()
+
+    render(
+      <AnimalCard animal={animal} containerClassName={containerClassName} />
+    )
+
+    expect(screen.getByTestId('container')).toHaveStyle({
+      marginTop: 8,
+    })
+  })
+})
