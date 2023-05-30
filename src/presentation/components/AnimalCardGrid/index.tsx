fb0cb4e59f55456efad5e74e5a5bbaff68f42dbf
Lucas Fernandes <lucas.fernandes.app@gmail.com>
2023-05-30 08:51:00 -0300

    feat: should show only cats cards

--- src/presentation/components/AnimalCardGrid/index.tsx
+++ src/presentation/components/AnimalCardGrid/index.tsx
@@ -1,5 +1,6 @@
 import { FlatList } from 'react-native'
 import type Animal from '../../../domain/models/Animal'
+import { useAnimalType } from '../../hooks/useAnimalType'
 import AnimalCard from '../AnimalCard'
 
 export interface AnimalCardGridProps {
@@ -7,10 +8,14 @@ export interface AnimalCardGridProps {
 }
 
 const AnimalCardGrid = ({ animals }: AnimalCardGridProps) => {
+  const [animalType] = useAnimalType()
+
+  const animalsFiltered = animals.filter((animal) => animal.type === animalType)
+
   return (
     <FlatList
       className="px-5 mb-5"
-      data={animals}
+      data={animalsFiltered}
       renderItem={({ item }) => (
         <AnimalCard testID="animalCard" className="mt-5" animal={item} />
       )}
